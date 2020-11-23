94959951853c8e46482668bc499271e07e9f956d
franklinjff <32177920+franklinjff@users.noreply.github.com>
2020-11-23 10:45:55 -0700

    Update network.tf
    Update private route table to point to NAT GW for outbound

--- Agent-Install-Examples/Terraform-bootstrap-s3/network.tf
+++ Agent-Install-Examples/Terraform-bootstrap-s3/network.tf
@@ -50,8 +50,8 @@ resource "aws_route_table" "private" {
 
   route {
     cidr_block     = "0.0.0.0/0"
-    #nat_gateway_id = aws_nat_gateway.nat_gw.id
-    gateway_id = aws_internet_gateway.gw.id
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+    #gateway_id = aws_internet_gateway.gw.id
 
   }
 }
