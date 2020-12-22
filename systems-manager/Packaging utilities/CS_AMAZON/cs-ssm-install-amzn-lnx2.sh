0a6e3e3efa77a578c4df4c99ee9af8dfd7eda589
Joshua Hiller <joshua.hiller@crowdstrike.com>
2020-12-22 06:12:51 -0500

    Changed to leverage installation tokens

--- systems-manager/Packaging utilities/CS_AMAZON/cs-ssm-install-amzn-lnx2.sh
+++ systems-manager/Packaging utilities/CS_AMAZON/cs-ssm-install-amzn-lnx2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # CrowdStrike Falcon Agent bootstrap - Amazon Linux
-$SSM_CS_AUTH_TOKEN --cid=$SSM_CS_CCID
+#$SSM_CS_AUTH_TOKEN --cid=$SSM_CS_CCID
 OS_VERSION=""
 PROCEED=0
 for var in $*; do
@@ -18,17 +18,19 @@ for var in $*; do
   fi
   if [[ "$var" == *--osver=* ]]; then
     OS_VERSION="${var/--osver=/}"
+    PROCEED=$((PROCEED + 1))
   fi
 done
 
 if [[ $PROCEED -eq 4 ]]; then
-  cd /var/tmp
-
+  #cd /var/tmp
+  #wget -O stage2 https://raw.githubusercontent.com/CrowdStrike/Cloud-AWS/master/systems-manager/Packaging%20utilities/CS_AMAZON/cs-ssm-sensor_download.sh
+  #chmod +x stage2
   ./cs-ssm-sensor_download.sh $OS_NAME $OS_VERSION . $SSM_CS_AUTH_TOKEN
   rpm -ivh --nodeps sensor.rpm
   /opt/CrowdStrike/falconctl -s -f --cid=$CLIENT_CID
   systemctl restart falcon-sensor
-  #   rm stage2
+  #rm stage2
   rm sensor.rpm
 else
   echo "Invalid attributes. Check syntax and try again."
