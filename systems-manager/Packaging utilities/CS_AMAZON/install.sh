1c17b78e46475111303b7aa08fb6b3afe3b620e5
Šimon Lukašík <isimluk@fedoraproject.org>
2021-01-15 13:10:56 +0000

    Golden & unified way to deploy falcon-sensor to the cloud (#21)
    * Golden & unified way to deploy falcon-sensor to the cloud
    
    The script
     - (1) fetches customer credentials from AWS SSM parameter store
     - (2) downloads falcon sensor from CrowdStrike
     - (3) sets-up the sensor
    
    I envision this script to be extended in future to be able to work with bundled
    falcon-sensor and/or credentials outside of AWS SSM parameter store.
    
    The script is compilation of existing prior work we had in this realm.
    Thank You guys!
    
    I think focusing on a single script that does the job well may turn out
    beneficial in many ways. Single script means less documentation and single thing
    to maintain.
    
    This is POSIX script (tested with bash and dash). Minimal set-up of tools need
    to be present on the system (curl, awk, grep). Tested on many platforms that are
    supported by AWS SSM and Falcon, but not on all.
    
    There is still work to be done on the script, but I think it is ready to be
    merged.
    
    * Support all linux platforms beyond amazon
    
    * Hide default (happy path) outputs from installers a bit
    
    Also hide apt-get noninteractive errors like:
    --------------- stderr ---------------
    install errors: debconf: unable to initialize frontend: Dialog
    debconf: (TERM is not set, so the dialog frontend is not usable.)
    debconf: falling back to frontend: Readline
    debconf: unable to initialize frontend: Readline
    debconf: (This frontend requires a controlling tty.)
    debconf: falling back to frontend: Teletype
    dpkg-preconfigure: unable to re-open stdin:
    
    Co-authored-by: Simon Lukasik <simon.lukasik@crowdstrike.com>

--- systems-manager/Packaging utilities/CS_AMAZON/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-chmod +x cs-ssm-install-amzn-lnx2.sh
-chmod +x cs-ssm-sensor_download.sh
-./cs-ssm-install-amzn-lnx2.sh --auth_token=$SSM_CS_AUTH_TOKEN --cid=$SSM_CS_CCID --os=amzn --osver=2
