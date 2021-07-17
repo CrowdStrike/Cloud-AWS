b787bf627628210ed7760157336f455ef4c043b2
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-07-17 12:18:46 -0400

    Removed legacy project

--- Falcon-Integration-Gateway/terraform/full/lambda-trigger-full.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_lambda_event_source_mapping" "event_source_mapping" {
-  event_source_arn = aws_sqs_queue.sqs_queue.arn
-  enabled          = true
-  function_name    = aws_lambda_function.fig_example_lambda.arn
-  batch_size       = 1
-}
