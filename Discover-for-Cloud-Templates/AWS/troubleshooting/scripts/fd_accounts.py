9bfcc84020ba24df751e2b8d27ea75b508dd7c0d
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-08-05 23:16:45 -0400

    Inactive account handling on check. Linting.

--- Discover-for-Cloud-Templates/AWS/troubleshooting/scripts/fd_accounts.py
+++ Discover-for-Cloud-Templates/AWS/troubleshooting/scripts/fd_accounts.py
@@ -9,14 +9,13 @@
 #########################################################################
 import argparse
 import json
-import sys
-import requests
-#Falcon SDK - All in one uber-class
+# Falcon SDK - All in one uber-class
 from falconpy import api_complete as FalconSDK
 
-############### FORMAT API PAYLOAD
+
+# ############## FORMAT API PAYLOAD
 def format_api_payload(rate_limit_reqs=0, rate_limit_time=0):
-    #Generates a properly formatted JSON payload for POST and PATCH requests
+    # Generates a properly formatted JSON payload for POST and PATCH requests
     data = {
         "resources": [
             {
@@ -32,104 +31,126 @@ def format_api_payload(rate_limit_reqs=0, rate_limit_time=0):
     }
     return data
 
-############### CHECK ACCOUNTS
+
+# ############## CHECK ACCOUNTS
 def check_account():
-    #Retrieve the account list
-    account_list = falcon.command(action="QueryAWSAccounts", parameters={"limit":"{}".format(str(query_limit))})["body"]["resources"]
-    #Log the results of the account query to a file if logging is enabled
+    # Retrieve the account list
+    account_list = falcon.command(action="QueryAWSAccounts",
+                                  parameters={"limit": "{}".format(str(query_limit))}
+                                  )["body"]["resources"]
+    # Log the results of the account query to a file if logging is enabled
     if log_enabled:
         with open('falcon-discover-accounts.json', 'w+') as f:
             json.dump(account_list, f)
-    #Create a list of our account IDs out of account_list
-    id_items=[]
+    # Create a list of our account IDs out of account_list
+    id_items = []
     for z in account_list:
         id_items.append(z["id"])
-    #id_list = ",".join([a["id"] for a in account_list])
-    #Returns the specified value for a specific account id within account_list
-    account_value = lambda i,v: [a[v] for a in account_list if a["id"] == i][0]
-    q_max=10    #VerifyAWSAccountAccess has a ID max count of 10
+    # Returns the specified value for a specific account id within account_list
+    account_value = lambda i, v: [a[v] for a in account_list if a["id"] == i][0]  # noqa: E731
+    q_max = 10    # VerifyAWSAccountAccess has a ID max count of 10
     for index in range(0, len(id_items), q_max):
         sub_acct_list = id_items[index:index + q_max]
         temp_list = ",".join([a for a in sub_acct_list])
-        #Check our AWS account access against the list of accounts returned in our query
+        # Check our AWS account access against the list of accounts returned in our query
         access_response = falcon.command(action="VerifyAWSAccountAccess", ids=temp_list)
-        if access_response['status_code'] == 200:
-            #Loop through each ID we verified
-            for result in access_response["body"]["resources"]:
+        if access_response['status_code'] == 200 or access_response['status_code'] == 409:
+            # Loop through each ID we verified
+            resource_list = access_response["body"]["resources"]
+            for result in [result for result in (resource_list or [])]:
                 if result["successful"]:
-                    #This account is correctly configured
+                    # This account is correctly configured
                     print(f'Account {result["id"]} is ok!')
                 else:
-                    #This account is incorrectly configured.  We'll use our account_value function to
-                    #retrieve configuration values from the account list we've already ingested.
+                    # This account is incorrectly configured.  We'll use our account_value function to
+                    # retrieve configuration values from the account list we've already ingested.
                     account_values_to_check = {
                         'id': result["id"],
-                        'iam_role_arn': account_value(result["id"],"iam_role_arn"),
-                        'external_id': account_value(result["id"],"external_id"),
-                        'cloudtrail_bucket_owner_id': account_value(result["id"],"cloudtrail_bucket_owner_id"),
-                        'cloudtrail_bucket_region': account_value(result["id"],"cloudtrail_bucket_region"),
+                        'iam_role_arn': account_value(result["id"], "iam_role_arn"),
+                        'external_id': account_value(result["id"], "external_id"),
+                        'cloudtrail_bucket_owner_id': account_value(result["id"], "cloudtrail_bucket_owner_id"),
+                        'cloudtrail_bucket_region': account_value(result["id"], "cloudtrail_bucket_region"),
                     }
-                    #Use the account_value function to retrieve the access_health branch, which contains our api failure reason
+                    # Use the account_value function to retrieve the
+                    # access_health branch, which contains our api failure reason
                     try:
-                        print(f'Account {result["id"]} has a problem: {account_value(result["id"],"access_health")["api"]["reason"]}')
-                    except:
-                        #The above call will produce an error if we're running check immediately after registering an account as 
-                        #the access_health branch hasn't been populated yet. Requery the API for the account_list when this happens.
-                        account_list = falcon.command(action="QueryAWSAccounts", parameters={"limit":"{}".format(str(query_limit))})["body"]["resources"]
-                        print(f'Account {result["id"]} has a problem: {account_value(result["id"],"access_health")["api"]["reason"]}')
-                    #Output the account details to the user to assist with troubleshooting the account
+                        problem = account_value(result["id"], "access_health")["api"]["reason"]
+                        print(f'Account {result["id"]} has a problem: {problem}')
+                    except:  # noqa: E722
+                        # The above call will produce an error if we're running
+                        # check immediately after registering an account as
+                        # the access_health branch hasn't been populated yet.
+                        # Requery the API for the account_list when this happens.
+                        account_list = falcon.command(action="QueryAWSAccounts",
+                                                      parameters={"limit": "{}".format(str(query_limit))}
+                                                      )["body"]["resources"]
+                        issue = account_value(result["id"], "access_health")["api"]["reason"]
+                        print(f'Account {result["id"]} has a problem: {issue}')
+                    # Output the account details to the user to assist with troubleshooting the account
                     print(f'Current settings {json.dumps(account_values_to_check, indent=4)}\n')
+            for status in [status for status in (access_response["body"]["errors"] or [])]:
+                # This account has an issue
+                print(f'Account {status["id"]} has an error: {status["message"]}!')
         else:
             try:
-                #An error has occurred
-                print("Got response error code {} message {}".format(access_response["status_code"], access_response["body"]["errors"][0]["message"]))
-            except:
-                #Handle any egregious errors that break our return error payload
+                # An error has occurred
+                print("Got response error code {}".format(access_response["status_code"]))
+            except:  # noqa: E722
+                # Handle any egregious errors that break our return error payload
                 print("Got response error code {} message {}".format(access_response["status_code"], access_response["body"]))
+                pass
 
     return
 
-############### REGISTER ACCOUNT
+
+# ############## REGISTER ACCOUNT
 def register_account():
-    #Call the API to update the requested account.
+    # Call the API to update the requested account.
     register_response = falcon.command(action="ProvisionAWSAccounts", parameters={}, body=format_api_payload())
     if register_response["status_code"] == 201:
         print("Successfully registered account.")
     else:
-        print("Registration failed with response: {} {}".format(register_response["status_code"], register_response["body"]["errors"][0]["message"]))
-    
+        print("Registration failed with response: {} {}".format(register_response["status_code"],
+                                                                register_response["body"]["errors"][0]["message"]
+                                                                ))
+
     return
 
-############### UPDATE ACCOUNT
+
+# ############## UPDATE ACCOUNT
 def update_account():
-    #Call the API to update the requested account.
+    # Call the API to update the requested account.
     update_response = falcon.command(action="UpdateAWSAccounts", body=format_api_payload())
     if update_response["status_code"] == 200:
         print("Successfully updated account.")
     else:
-        print("Update failed with response: {} {}".format(update_response["status_code"], update_response["body"]["errors"][0]["message"]))
-    
+        print("Update failed with response: {} {}".format(update_response["status_code"],
+                                                          update_response["body"]["errors"][0]["message"]))
+
     return
 
-############### DELETE ACCOUNT
+
+# ############## DELETE ACCOUNT
 def delete_account():
-    #Call the API to delete the requested account, multiple IDs can be deleted by passing in a comma-delimited list
+    # Call the API to delete the requested account, multiple IDs can be deleted by passing in a comma-delimited list
     delete_response = falcon.command(action="DeleteAWSAccounts", parameters={}, ids=local_account)
     if delete_response["status_code"] == 200:
         print("Successfully deleted account.")
     else:
-        print("Delete failed with response: {} {}".format(delete_response["status_code"], delete_response["body"]["errors"][0]["message"]))
-    
+        print("Delete failed with response: {} {}".format(delete_response["status_code"],
+                                                          delete_response["body"]["errors"][0]["message"]))
+
     return
 
-############### MAIN
+
+# ############## MAIN
 if __name__ == "__main__":
-    #Configure argument parsing
+    # Configure argument parsing
     parser = argparse.ArgumentParser(description="Get Params to send notification to CRWD topic")
-    #Fully optional
+    # Fully optional
     parser.add_argument('-q', '--query_limit', help='The query limit used for check account commands', required=False)
     parser.add_argument('-l', '--log_enabled', help='Save results to a file?', required=False, action="store_true")
-    #Optionally required
+    # Optionally required
     parser.add_argument('-r', '--cloudtrail_bucket_region', help='AWS Region where the S3 bucket is hosted',
                         required=False)
     parser.add_argument('-o', '--cloudtrail_bucket_owner_id', help='Account where the S3 bucket is hosted',
@@ -138,21 +159,24 @@ if __name__ == "__main__":
     parser.add_argument('-e', '--external_id', help='External ID used to assume role in account', required=False)
     parser.add_argument('-i', '--iam_role_arn',
                         help='IAM AWS IAM Role ARN that grants access to resources for Crowdstrike', required=False)
-    #Always required
+    # Always required
     parser.add_argument('-c', '--command', help='Troubleshooting action to perform', required=True)
     parser.add_argument("-f", "--falcon_client_id", help="Falcon Client ID", required=True)
-    parser.add_argument("-s", "--falcon_client_secret", help="Falcon Client Secret", required=True)    
+    parser.add_argument("-s", "--falcon_client_secret", help="Falcon Client Secret", required=True)
     args = parser.parse_args()
 
-    ############### SET GLOBALS
+    # ############## SET GLOBALS
     command = args.command
-    #Only execute our defined commands        
+    # Only execute our defined commands
     if command.lower() in "check,update,register,delete":
         if command.lower() in "update,register":
-            #All fields required for update and register
-            if (args.cloudtrail_bucket_owner_id is None or args.cloudtrail_bucket_region is None
-                or args.local_account is None or args.external_id is None or args.iam_role_arn is None):
-                    parser.error("The {} command requires the -r, -o, -a, -e, -i arguments to also be specified.".format(command))
+            # All fields required for update and register
+            if (args.cloudtrail_bucket_owner_id is None
+                    or args.cloudtrail_bucket_region is None
+                    or args.local_account is None
+                    or args.external_id is None
+                    or args.iam_role_arn is None):
+                parser.error("The {} command requires the -r, -o, -a, -e, -i arguments to also be specified.".format(command))
             else:
                 cloudtrail_bucket_region = args.cloudtrail_bucket_region
                 cloudtrail_bucket_owner_id = args.cloudtrail_bucket_owner_id
@@ -160,14 +184,14 @@ if __name__ == "__main__":
                 external_id = args.external_id
                 iam_role_arn = args.iam_role_arn
         elif command.lower() in "delete":
-            #Delete only requires the local account ID
+            # Delete only requires the local account ID
             if args.local_account is None:
                 parser.error("The {} command requires the -l argument to also be specified.".format(command))
             else:
                 local_account = args.local_account
     else:
-        parser.error("The {} command is not recognized.".format(command))    
-    #These globals exist for all requests
+        parser.error("The {} command is not recognized.".format(command))
+    # These globals exist for all requests
     falcon_client_id = args.falcon_client_id
     falcon_client_secret = args.falcon_client_secret
     log_enabled = args.log_enabled
@@ -176,26 +200,30 @@ if __name__ == "__main__":
     else:
         query_limit = args.query_limit
 
-    ############### MAIN ROUTINE
-    #Connect to the API using our provided falcon client_id and client_secret
+    # ############## MAIN ROUTINE
+    # Connect to the API using our provided falcon client_id and client_secret
     try:
-        falcon = FalconSDK.APIHarness(creds={'client_id': falcon_client_id,'client_secret': falcon_client_secret})
-    except:
-        #We can't communicate with the endpoint
+        falcon = FalconSDK.APIHarness(creds={'client_id': falcon_client_id, 'client_secret': falcon_client_secret})
+    except:  # noqa: E722
+        # We can't communicate with the endpoint
         print("Unable to communicate with API")
-    #Authenticate
+    # Authenticate
     if falcon.authenticate():
         try:
-            #Execute the command by calling the named function
-            exec("{}_account()".format(command.lower()))
+            # Execute the command by calling the named function
+            if command.lower() == "check":
+                check_account()
+            if command.lower() == "update":
+                update_account()
+            if command.lower() == "register":
+                register_account()
+            if command.lower() == "delete":
+                delete_account()
         except Exception as e:
-            #Handle any previously unhandled errors
+            # Handle any previously unhandled errors
             print("Command failed with error: {}.".format(str(e)))
-        #Discard our token before we exit
+        # Discard our token before we exit
         falcon.deauthenticate()
     else:
-        #Report that authentication failed and stop processing
+        # Report that authentication failed and stop processing
         print("Authentication Failure.")
-        
-    #Force clean exit
-    sys.exit(0)
