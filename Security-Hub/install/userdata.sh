b5f1dcb9a7448db62da5a49aba3f4d925da9c975
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-08-05 21:18:16 -0400

    Example update to point to new repo

--- Security-Hub/install/userdata.sh
+++ Security-Hub/install/userdata.sh
@@ -3,6 +3,6 @@
 # This is the userdata script for newly created EC2 instances. 
 # This script should not be executed manually
 cd /var/tmp
-wget -O ${FigFileName} https://raw.githubusercontent.com/CrowdStrike/Cloud-AWS/master/Falcon-Integration-Gateway/install/${FigFileName}
+wget -O ${FigFileName} https://raw.githubusercontent.com/CrowdStrike/Cloud-AWS/master/Security-Hub/install/${FigFileName}
 chmod 755 ${FigFileName}
 ./${FigFileName} --target /usr/share/fig
