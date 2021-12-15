e1868f09c47a3ae9fbfa27b519cd94005db101bb
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-12-14 22:51:13 -0500

    Linting

--- Security-Hub/logger.py
+++ Security-Hub/logger.py
@@ -1,9 +1,6 @@
-# logger.py
-# python 3.7
+"""Log handler for Security Hub integration"""
 # DCS 9/17/18
 # JSH 11/15/20 modified
-
-# logger.py opens a local logfile for write and writes logs to it
 
 import time
 import json
@@ -12,43 +9,47 @@ from logging.handlers import RotatingFileHandler
 
 
 class Logger():
+    """Class to handle all logging operations."""
     def __init__(self, logfile, logger):
-        """
-        Creates a rotating output log
-        """
+        """Create a rotating output log."""
         self.outputlog = logging.getLogger(logger)
         self.outputlog.setLevel(logging.INFO)
         # add a rotating handler
         handler = RotatingFileHandler(logfile, maxBytes=20971520, backupCount=5)
         self.outputlog.addHandler(handler)
 
-    def statusWrite(self, log):
+    def status_write(self, log):
+        """Write a status message to the log."""
         # write will take as input an execution log and write it to the log along with the time
         # @params log: the log to write to the file
-        ts = time.ctime()
-        self.outputlog.info(ts + " %s\n" % log)
+        timestamp = time.ctime()
+        self.outputlog.info(timestamp + " %s\n" % log)
 
-    def outputWrite(self, log):
+    def output_write(self, log):
+        """Write a log to the log."""
         # write will take as input an output log and write it to the log along with the time
         # @params log: the log to write to the file
-        ts = time.ctime()
-        self.outputlog.info(ts + "\n ")
+        timestamp = time.ctime()
+        self.outputlog.info("%s\n ", timestamp)
         self.outputlog.info(json.dumps(log, indent=4))
 
-    def offsetWrite(self, offset_file, log):
+    @staticmethod
+    def offset_write(offset_file, log):
+        """Log the offset."""
         # write will take as input an offset log and write it to the log along with the time
         # @params log: the log to write to the file
-        logfile = open(offset_file, "w")
-        logfile.write(str(log))
-        logfile.close()
+        with open(offset_file, "w", encoding="utf-8") as logfile:
+            logfile.write(str(log))
 
-    def offsetRead(self, offset_file):
+    @staticmethod
+    def offset_read(offset_file):
+        """Read the offset."""
         offset = 0
         try:
-            logfile = open(offset_file, "r")
-            offset = int(logfile.readline())
-            logfile.close()
-        except:
+            with open(offset_file, "r", encoding="utf-8") as logfile:
+                offset = int(logfile.readline())
+        except (FileNotFoundError, OSError, ValueError):
+            # Skip if we can't find the file or read it, we'll start at zero.
             pass
 
         return offset
