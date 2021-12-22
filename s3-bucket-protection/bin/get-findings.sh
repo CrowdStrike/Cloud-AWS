af37cf6d5101d9bf63df3eaa9909eccf6b1736f2
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-12-21 19:27:32 -0500

    Fix interpolation

--- s3-bucket-protection/bin/get-findings.sh
+++ s3-bucket-protection/bin/get-findings.sh
@@ -2,7 +2,7 @@
 echo -e "\nFindings in $(echo ${BUCKET/s3:\/\//}) for the past hour"
 st=$(date -d '1 hour ago' '+%Y-%m-%dT%H:%M:%SZ')
 en=$(date -d 'now' '+%Y-%m-%dT%H:%M:%SZ')
-FILTER='{"ResourceId":[{"Value":"'+$(echo ${BUCKET/s3:\/\//})+'","Comparison":"PREFIX"}]'
+FILTER='{"ResourceId":[{"Value":"'$(echo ${BUCKET/s3:\/\//})'","Comparison":"PREFIX"}]'
 FILTER="$FILTER,\"CreatedAt\":[{\"Start\":\"$st\",\"End\":\"$en\"}]}"
 REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq .region -r)
 aws securityhub get-findings --region=$REGION \
