71db91cd44a999382b5805364f17b5b73d5ebc56
Joshua Hiller <joshua.hiller@crowdstrike.com>
2023-03-28 10:56:25 -0400

    Add quota check.  Logical refactor. Closes #217.

--- s3-bucket-protection/lambda/lambda_function.py
+++ s3-bucket-protection/lambda/lambda_function.py
@@ -1,7 +1,19 @@
-"""CrowdStrike S3 Bucket Protection with QuickScan.
+r"""CrowdStrike S3 Bucket Protection with QuickScan.
 
-Creation date: 09.01.21 - jshcodes@CrowdStrike
-Modification: 12.21.21 - jshcodes@CrowdStrike
+ _______                        __ _______ __        __ __
+|   _   .----.-----.--.--.--.--|  |   _   |  |_.----|__|  |--.-----.
+|.  1___|   _|  _  |  |  |  |  _  |   1___|   _|   _|  |    <|  -__|
+|.  |___|__| |_____|________|_____|____   |____|__| |__|__|__|_____|
+|:  1   |                         |:  1   |
+|::.. . |                         |::.. . |
+`-------'                         `-------'
+
+Creation     : 09.01.21 - jshcodes@CrowdStrike, v1.0
+Modification : 12.21.21 - jshcodes@CrowdStrike, v1.1
+Modification : 03.21.23 - jshcodes@CrowdStrike, v1.2
+
+This lambda handler requires the CrowdStrike FalconPy lambda layer.
+Download the latest version: https://falconpy.io/downloads/falconpy-layer.zip
 """
 import io
 import os
@@ -12,9 +24,14 @@ import json
 import boto3
 from botocore.exceptions import ClientError
 # FalconPy SDK - Auth, Sample Uploads and Quick Scan
-from falconpy import OAuth2, SampleUploads, QuickScan  # pylint: disable=E0401
-from functions import generate_manifest, send_to_security_hub
+try:
+    from falconpy import OAuth2, SampleUploads, QuickScan
+except ImportError as no_falconpy:
+    raise SystemExit("The crowdstrike-falconpy layer is not available") from no_falconpy
+from functions import generate_manifest, send_to_security_hub, check_quota
 
+# Current version
+APP_VERSION = 1.2
 
 # Maximum file size for scan (35mb)
 MAX_FILE_SIZE = 36700160
@@ -37,7 +54,8 @@ MITIGATE = bool(json.loads(os.environ.get("MITIGATE_THREATS", "TRUE").lower()))
 try:
     BASE_URL = os.environ["BASE_URL"]
 except KeyError:
-    BASE_URL = "https://api.crowdstrike.com"
+    # Cloud region autodiscovery will not work in GovCloud, specify usgov1 for BASE_URL instead
+    BASE_URL = "auto"
 
 # Grab our SSM parameter store variable names from the environment if they exist
 try:
@@ -65,10 +83,11 @@ except KeyError as bad_creds:
     raise SystemExit("Unable to retrieve CrowdStrike Falcon API credentials.") from bad_creds
 
 # Authenticate to the CrowdStrike Falcon API
-auth = OAuth2(creds={
-            "client_id": client_id,
-            "client_secret": client_secret
-            }, base_url=BASE_URL)
+auth = OAuth2(client_id=client_id,
+              client_secret=client_secret,
+              base_url=BASE_URL,
+              user_agent=f"crowdstrike-s3protection/{APP_VERSION}"
+              )
 
 # Connect to the Samples Sandbox API
 Samples = SampleUploads(auth_object=auth)
@@ -79,109 +98,115 @@ Scanner = QuickScan(auth_object=auth)
 # Main routine
 def lambda_handler(event, _):  # pylint: disable=R0912,R0914,R0915
     """Lambda execution entry point."""
+    returned = ""
     bucket_name = event['Records'][0]['s3']['bucket']['name']
     bucket = s3.Bucket(bucket_name)
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     upload_file_size = int(
         bucket.Object(key=key).get()["ResponseMetadata"]["HTTPHeaders"]["content-length"]
         )
-    if upload_file_size < MAX_FILE_SIZE:  # pylint: disable=R1702  # (6 is fine)
-        try:
-            filename = os.path.basename(key)
-            response = Samples.upload_sample(file_name=filename,
-                                             file_data=io.BytesIO(
-                                                 bucket.Object(key=key).get()["Body"].read()
-                                                 )
-                                             )
-        except Exception as err:
-            print(f"Error uploading object {key} from bucket {bucket_name} to Falcon X Sandbox. "
-                  "Make sure your API key has the Sample Uploads permission.")
-            raise err
+    if upload_file_size > MAX_FILE_SIZE:
+        returned = f"File ({key}) exceeds maximum file scan size ({MAX_FILE_SIZE} bytes), skipped."
+        log.warning(returned)
+        raise SystemExit(returned)
 
-        try:
-            # Uploaded file unique identifier
-            upload_sha = response["body"]["resources"][0]["sha256"]
-            # Scan request ID, generated when the request for the scan is made
-            scan_id = Scanner.scan_samples(body={"samples": [upload_sha]})["body"]["resources"][0]
-            scanning = True
-            # Loop until we get a result or the lambda times out
-            while scanning:
-                # Retrieve our scan using our scan ID
-                scan_results = Scanner.get_scans(ids=scan_id)
-                try:
+    if not check_quota(Scanner):
+        returned = "QuickScan quota exceeded. Unable to continue."
+        log.error(returned)
+        raise SystemExit(returned)
+
+    try:
+        filename = os.path.basename(key)
+        response = Samples.upload_sample(file_name=filename,
+                                         file_data=io.BytesIO(
+                                            bucket.Object(key=key).get()["Body"].read()
+                                            )
+                                         )
+    except Exception as err:
+        returned = f"Error uploading {key} from bucket {bucket_name} to Falcon Intelligence Sandbox. "
+        returned = f"{returned}Make sure your API key has the Sample Uploads WRITE scope."
+        log.error(returned)
+        raise SystemExit(returned)
+
+    try:
+        # Uploaded file unique identifier
+        upload_sha = response["body"]["resources"][0]["sha256"]
+        # Scan request ID, generated when the request for the scan is made
+        scan_id = Scanner.scan_samples(samples=upload_sha)["body"]["resources"][0]
+        scanning = True
+        # Loop until we get a result or the lambda times out
+        while scanning:
+            # Retrieve our scan using our scan ID
+            scan_results = Scanner.get_scans(ids=scan_id)
+            try:
+                if scan_results["body"]["resources"]:
                     if scan_results["body"]["resources"][0]["status"] == "done":
                         # Scan is complete, retrieve our results (there will be only one)
                         result = scan_results["body"]["resources"][0]["samples"][0]
                         # and break out of the loop
                         scanning = False
-                    else:
-                        # Not done yet, sleep for a bit
-                        time.sleep(3)
-                except IndexError:
-                    # Results aren't populated yet, skip
-                    pass
-            if result["sha256"] == upload_sha:
-                if "no specific threat" in result["verdict"]:
-                    # File is clean
-                    scan_msg = f"No threat found in {key}"
-                    log.info(scan_msg)
-                elif "unknown" in result["verdict"]:
-                    if "error" in result:
-                        # Error occurred
-                        scan_msg = f"Scan error for {key}: {result['error']}"
-                        log.info(scan_msg)
-                    else:
-                        # Undertermined scan failure
-                        scan_msg = f"Unable to scan {key}"
-                        log.info(scan_msg)
-                elif "malware" in result["verdict"]:
-                    # Mitigation would trigger from here
-                    scan_msg = f"Verdict for {key}: {result['verdict']}"
-                    detection = {}
-                    detection["sha"] = upload_sha
-                    detection["bucket"] = bucket_name
-                    detection["file"] = key
-                    log.warning(scan_msg)
-                    threat_removed = False
-                    if MITIGATE:
-                        # Remove the threat
-                        try:
-                            threat = s3.Object(bucket_name, key)
-                            threat.delete()
-                            threat_removed = True
-                        except ClientError as err:
-                            log.warning("Unable to remove threat %s from bucket %s", key, bucket_name)
-                            print(f"{err}")
-                    else:
-                        # Mitigation is disabled. Complain about this in the log.
-                        log.warning("Threat discovered (%s). Mitigation disabled, threat persists in %s bucket.",
-                                    key,
-                                    bucket_name
-                                    )
-                    # Inform Security Hub of the threat and our mitigation status
-                    manifest = generate_manifest(detection, region, threat_removed)
-                    _ = send_to_security_hub(manifest, region)
+                # Not done yet, sleep for a bit
+                time.sleep(3)
+            except IndexError:
+                # Results aren't populated yet, wait a sec and try again
+                time.sleep(1)
+        if result["sha256"] == upload_sha:
+            if "no specific threat" in result["verdict"]:
+                # File is clean
+                returned = f"No threat found in {key}"
+                log.info(returned)
+            elif "unknown" in result["verdict"]:
+                if "error" in result:
+                    # Error occurred
+                    returned = f"Scan error for {key}: {result['error']}"
                 else:
-                    # Unrecognized response
-                    scan_msg = f"Unrecognized response ({result['verdict']}) received from API for {key}."
-                    log.info(scan_msg)
+                    # Undertermined scan failure
+                    returned = f"Unable to scan {key}"
+                log.warning(returned)
+            elif "malware" in result["verdict"]:
+                # Mitigation triggers from here
+                returned = f"Verdict for {key}: {result['verdict']}"
+                detection = {}
+                detection["sha"] = upload_sha
+                detection["bucket"] = bucket_name
+                detection["file"] = key
+                log.critical(returned)
+                threat_removed = False
+                if MITIGATE:
+                    # Remove the threat
+                    try:
+                        s3.Object(bucket_name, key).delete()
+                        threat_removed = True
+                    except ClientError as err:
+                        log.error("Unable to remove threat %s from bucket %s", key, bucket_name)
+                        print(err)
+                else:
+                    # Mitigation is disabled. Complain about this in the log.
+                    log.warning("Threat discovered (%s). Mitigation disabled, threat persists in %s bucket.",
+                                key,
+                                bucket_name
+                                )
+                # Inform Security Hub of the threat and our mitigation status
+                manifest = generate_manifest(detection, region, threat_removed)
+                _ = send_to_security_hub(manifest, region)
+            else:
+                # Unrecognized response
+                returned = f"Unrecognized response ({result['verdict']}) received from API for {key}."
+                log.warning(returned)
 
-            # Clean up the artifact in the sandbox
-            response = Samples.delete_sample(ids=upload_sha)
-            if response["status_code"] > 201:
-                log.warning("Could not remove sample (%s) from sandbox.", key)
+        # Clean up the artifact in the sandbox
+        response = Samples.delete_sample(ids=upload_sha)
+        if response["status_code"] > 201:
+            log.warning("Could not remove sample (%s) from sandbox.", key)
 
-            return scan_msg
-        except Exception as err:
-            print(err)
-            print(f"Error getting object {key} from bucket {bucket_name}. "
-                  "Make sure they exist and your bucket is in the same region as this function.")
-            raise err
+    except Exception as err:
+        returned = f"General error processing object {key} from bucket {bucket_name}. "
+        returned = f"{returned}Make sure they exist and your bucket is in the same region as this function."
+        returned = f"{returned}\n{err}"
+        log.error(returned)
+        raise SystemExit(returned)
 
-    else:
-        msg = f"File ({key}) exceeds maximum file scan size ({MAX_FILE_SIZE} bytes), skipped."
-        log.warning(msg)
-        return msg
+    return returned
 
     #           ██████
     #         ██      ██
