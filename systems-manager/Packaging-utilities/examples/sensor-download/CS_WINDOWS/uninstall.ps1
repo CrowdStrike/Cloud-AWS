45443956fd2871de7d632319aadca78333f6bd30
ffalor <35144141+ffalor@users.noreply.github.com>
2023-04-25 16:52:27 -0500

    Update uninstall.ps1

--- systems-manager/Packaging-utilities/examples/sensor-download/CS_WINDOWS/uninstall.ps1
+++ systems-manager/Packaging-utilities/examples/sensor-download/CS_WINDOWS/uninstall.ps1
@@ -4,6 +4,9 @@ param()
 
 Write-Output 'Uninstalling Falcon Sensor...'
 
+# Configures the TLS version to use for secure connections
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 $agentService = Get-Service -Name CSAgent -ErrorAction SilentlyContinue
 if (-not $agentService) {
   Write-Output 'CSAgent service not installed...'
@@ -17,30 +20,40 @@ $package = Get-Package -Name 'CrowdStrike Windows Sensor'
 $uninstallString = $package.Metadata['BundleCachePath']
 
 # Sets up the arguments to be passed to the uninstall executable
-$uninstallArgs = '/uninstall /quiet'
+$uninstallArgs = '/uninstall /quiet' + $env:UNINSTALLPARAMS
+
+if ($env:MAINTENANCE_TOKEN) {
+    $uninstallArgs += ' MAINTENANCE_TOKEN=' + $env:MAINTENANCE_TOKEN
+}
 
 # Starts the uninstall process and waits for it to complete
+Write-Output "Uninstalling with arguments: $uninstallArgs"
 $uninstallProcess = Start-Process -FilePath $uninstallString -ArgumentList $uninstallArgs -PassThru -Wait
 
+Write-Output $uninstallProcess
 # Checks the exit code of the uninstall process and throws an exception if it is not 0
 if ($uninstallProcess.ExitCode -ne 0) {
-  throw "Uninstaller returned exit code $($UninstallerProcess.ExitCode)"
+  Write-Output "Failed to uninstall with exit code: $($uninstallProcess.ExitCode)"
+  exit 1
 }
 
 # Retrieves the status of the CSAgent service and throws an exception if it is still running after the uninstall
 $agentService = Get-Service -Name CSAgent -ErrorAction SilentlyContinue
 if ($agentService -and $agentService.Status -eq 'Running') {
-  throw 'Service uninstall failed...'
+  Write-Output 'Uninstall process completed, but CSAgent service is still running. Uninstall failed for unknown reason...'
+  exit 1 
 }
 
 # Checks if the CrowdStrike registry key was successfully removed and throws an exception if it still exists
 if (Test-Path -Path HKLM:\System\Crowdstrike) {
-  throw 'Registry key removal failed...'
+  Write-Output "CrowdStrike registry key still exists. Uninstall failed."
+  exit 1
 }
 
 # Checks if the CrowdStrike driver was successfully removed and throws an exception if it still exists
 if (Test-Path -Path "${env:SYSTEMROOT}\System32\drivers\CrowdStrike") {
-  throw 'Driver removal failed...'
+  Write-Output "CrowdStrike driver still exists. Uninstall failed."
+  exit 1
 }
 
 Write-Output 'Successfully finished uninstall...'
