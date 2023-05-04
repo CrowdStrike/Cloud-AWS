7e7f9ec08bba911af8a8995e973ea5562da34603
Joshua Hiller <joshua.hiller@crowdstrike.com>
2023-05-04 10:39:40 -0400

    Use custom FalconPy layer. Increase warmup timeout. Relates to #230.

--- s3-bucket-protection/demo.sh
+++ s3-bucket-protection/demo.sh
@@ -39,16 +39,17 @@ then
 	read -p "EC2 Instance Key Name: " ECKEY
 	read -p "Trusted IP address: " TRUSTED
     UNIQUE=$(echo $RANDOM | md5sum | sed "s/[[:digit:].-]//g" | head -c 8)
-    rm lambda/falconpy-layer.zip >/dev/null 2>&1
-    curl -o lambda/falconpy-layer.zip https://falconpy.io/downloads/falconpy-layer.zip
+    # This demo will be using a custom version of the falconpy layer for now. - jshcodes@CrowdStrike 05.04.2023 #230
+    #rm lambda/falconpy-layer.zip >/dev/null 2>&1
+    #curl -o lambda/falconpy-layer.zip https://falconpy.io/downloads/falconpy-layer.zip
     if ! [ -f demo/.terraform.lock.hcl ]; then
         terraform -chdir=demo init
     fi
 	terraform -chdir=demo apply -compact-warnings --var falcon_client_id=$FID \
 		--var falcon_client_secret=$FSECRET --var instance_key_name=$ECKEY \
 		--var trusted_ip=$TRUSTED/32 --var unique_id=$UNIQUE --auto-approve
-    echo -e "$RD\nPausing for 30 seconds to allow configuration to settle.$NC"
-    sleep 30
+    echo -e "$RD\nPausing for 60 seconds to allow configuration to settle.$NC"
+    sleep 60
     all_done
 	exit 0
 fi
