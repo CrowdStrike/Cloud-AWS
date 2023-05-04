fdc296e577177f6dc792adacb8b9916e7d025b0b
Joshua Hiller <joshua.hiller@crowdstrike.com>
2023-05-04 10:38:24 -0400

    Add AWSLambdaPowertoolsPythonV2 layer, Python 3.10. Relates to #230.

--- s3-bucket-protection/demo/lambda-function.tf
+++ s3-bucket-protection/demo/lambda-function.tf
@@ -20,11 +20,21 @@ data "archive_file" "lambda_archive" {
     var.lambda_function_filename
     ]
 }
+variable "python_tools_layer_name" {
+    type = string
+    default = "AWSLambdaPowertoolsPythonV2"
+}
+data "aws_lambda_layer_version" "pythontools" {
+    layer_name = "arn:aws:lambda:${var.region}:017000801446:layer:${var.python_tools_layer_name}"
+    version = 30
+}
+
 resource "aws_lambda_layer_version" "falconpy" {
   filename      = "${path.cwd}/lambda/${var.falconpy_layer_filename}"
   layer_name    = "${var.unique_id}_${var.falconpy_layer_name}"
-  compatible_runtimes = ["python3.7", "python3.8"]
+  compatible_runtimes = ["python3.10"]
 }
+
 resource "aws_lambda_permission" "allow_bucket" {
   statement_id  = "AllowExecutionFromS3Bucket"
   action        = "lambda:InvokeFunction"
@@ -39,8 +49,8 @@ resource "aws_lambda_function" "func" {
   description   = var.lambda_description
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda_function.lambda_handler"
-  layers        = [aws_lambda_layer_version.falconpy.arn]
-  runtime       = "python3.8"
+  layers        = [data.aws_lambda_layer_version.pythontools.arn, aws_lambda_layer_version.falconpy.arn]
+  runtime       = "python3.10"
   timeout       = 30
   depends_on = [data.archive_file.lambda_archive]
   environment {
