45e4270465484b541e947294c7cceb36d3384af8
Gabe Alford <redhatrises@gmail.com>
2025-08-25 12:00:20 -0600

    Remove deprecated QS integration

--- s3-bucket-protection/bin/upload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Uploading test files, please wait..."
-for i in $(ls /home/ec2-user/testfiles)
-do
-    aws s3 cp /home/ec2-user/testfiles/$i $BUCKET/$i
-done
-echo "Upload complete. Check CloudWatch logs or use the get-findings command for scan results."
--- s3-bucket-protection/demo/output.tf
+++
@@ -1,6 +0,0 @@
-output "demo_bucket" {
-	value = "s3://${aws_s3_bucket.bucket.id}"
-}
-output "demo_instance" {
-	value = "ec2-user@${aws_instance.amzn_instance.public_ip}"
-}
